28b5cee943f7c469e972a7a6912423a5f4744741
Agatha Camargo <agathinhas.c0@gmail.com>
2025-08-13 08:38:41 -0300

    aula 12.08

---
+++ Aulas-12.08.2025/Assets/aula - script/andadinha.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float velocidadeAndar = 5f;
+    public float velocidadeVoo = 8f;
+
+   
+    private bool estaVoando = false;
+
+   
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+     
+        rb = GetComponent<Rigidbody2D>();
+       
+    }
+
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            estaVoando = !estaVoando; 
+
+           
+            rb.gravityScale = estaVoando ? 0 : 1;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+        if (estaVoando)
+        {
+            MovimentoVoo();
+        }
+        else
+        {
+            MovimentoAndar();
+        }
+    }
+
+    private void MovimentoAndar()
+    {
+       
+        float moveInput = Input.GetAxis("Horizontal");
+
+       
+        rb.velocity = new Vector2(moveInput * velocidadeAndar, rb.velocity.y);
+    }
+
+    private void MovimentoVoo()
+    {
+        
+        float moveInputHorizontal = Input.GetAxis("Horizontal");
+        float moveInputVertical = Input.GetAxis("Vertical");
+
+       
+        Vector2 movimento = new Vector2(moveInputHorizontal, moveInputVertical);
+
+        
+        rb.velocity = movimento * velocidadeVoo;
+    }
+}
